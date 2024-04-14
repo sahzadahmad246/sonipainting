1780a7677bfe8da22a10736633af96ff6e090b10
sahzadahmad246 <124631079+sahzadahmad246@users.noreply.github.com>
2024-04-14 17:27:50 +0530

    commit

--- src/Components/pages/Gallery.jsx
+++ src/Components/pages/Gallery.jsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import ImagePreview from "../other/ImagePreview";
 import "../../CSS/home/Gallery.css";
 import { ThreeDots } from "react-loader-spinner";
-import axios from "axios";
+// import axios from "axios"; // Comment out axios import
+import image1 from "../../images/image1.jpg";
+import image2 from "../../images/image2.jpg";
+import image3 from "../../images/image3.jpg";
+import image4 from "../../images/image4.jpg";
+import image5 from "../../images/image5.jpg";
+import image6 from "../../images/image6.jpg";
 
 const Gallery = ({ displayCount }) => {
   // Receive displayCount prop
@@ -11,20 +17,26 @@ const Gallery = ({ displayCount }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await axios.get(
-          "https://sonipainting-backend.onrender.com/get-images"
-        );
-        setImages(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching images:", error);
-        setLoading(false);
-      }
-    };
+    // const fetchImages = async () => {
+    //   try {
+    //     const response = await axios.get(
+    //       "https://sonipainting-backend.onrender.com/get-images"
+    //     );
+    //     setImages(response.data);
+    //     setLoading(false);
+    //   } catch (error) {
+    //     console.error("Error fetching images:", error);
+    //     setLoading(false);
+    //   }
+    // };
 
-    fetchImages();
+    // fetchImages();
+    
+    // Simulating API call delay with setTimeout
+    setTimeout(() => {
+      setImages([image1, image2, image3, image4, image5, image6]);
+      setLoading(false);
+    }, 1000); // Adjust timeout as needed
   }, []);
 
   const handleImageClick = (imageUrl) => {
@@ -54,16 +66,12 @@ const Gallery = ({ displayCount }) => {
             />
           </div>
         ) : (
-          images.slice(0, displayCount).map((imageName, index) => (
+          images.slice(0, displayCount).map((image, index) => (
             <div className="gallery-image" key={index}>
               <img
-                src={`https://sonipainting-backend.onrender.com/images/${imageName}`}
+                src={image} // Use local image source
                 alt={`Image ${index}`}
-                onClick={() =>
-                  handleImageClick(
-                    `https://sonipainting-backend.onrender.com/images/${imageName}`
-                  )
-                }
+                onClick={() => handleImageClick(image)}
               />
             </div>
           ))
